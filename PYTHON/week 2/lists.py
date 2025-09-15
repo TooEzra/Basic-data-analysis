25c2caf791fb9df2a3721b59b345fed39cd0ba5f
Ezra Kipkoech <147641185+TooEzra@users.noreply.github.com>
2025-09-15 23:19:41 +0300

    Delete PYTHON directory

--- PYTHON/week 2/lists.py
+++
@@ -1,5 +0,0 @@
-my_list = ['Ubuntu', 'Python', 'Community']
-my_list.append('Harmony') # Everyone has a place!
-print(my_list)  # Output: ['Ubuntu', 'Python', 'Community', 'Harmony']
-
-#For ordered collections of items that you frequently need to add, remove, or modify. Think of flexible, dynamic groupings.
